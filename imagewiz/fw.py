6001d19e92254ce6f46c9eec51f019667ecd14a3
evangibson <36212755+evangibson@users.noreply.github.com>
2019-08-07 20:03:38 -0400

    0.4 build/ added dicomtoPNG and weirdWrang

--- imagewiz/fw.py
+++ imagewiz/fw.py
@@ -8,7 +8,7 @@ Name: FileWiz (fw)
 Consideration: The imagewiz module is deisgned to work with established image processing python modules.
 ==========
 Description: This module is meant to be used to streamline the practical workflow of image preprocessing. 
-             This script deals with file movement, environment setup, and with some exploration of images.
+             This script deals with file movement, environment setup, and with reformatting unruly data.
 ==========
 Invokation: import imagewiz.fw as fw
 =========
@@ -24,11 +24,16 @@ import os
 import shutil
 from sklearn.model_selection import train_test_split
 import tqdm
+import numpy as np
+import png
+import pydicom
+
 
 #%% Definitions
 
 class fileWiz:
-    """Class dealing with the process of arranging files into directories for Tensorflow training"""
+    """Class dealing with the process of arranging files
+    into directories for Tensorflow training"""
     
     def classGrab(df,
                   pki_index_col,
@@ -60,7 +65,11 @@ class fileWiz:
                    extension_type = ".png",
                    repo_folder = str(os.getcwd()),
                    activate = False):
-        
+        """Takes files from one folder,
+        determines their classes using a pandas dataframe with a 'class' column
+        and a pki index, splits them into training and validation sets,
+        and places them into folders by training/validation and class.
+        """
         #creating training and validation
         train, val = train_test_split(df, test_size=tra_t_split)
         
@@ -115,3 +124,43 @@ class fileWiz:
                         pass
             
         return filename_list_train, filename_list_orig   
+
+class weirdWrang:
+    """Built for dealing with images that behave strangely
+    or need to be reformatted before modeling"""
+    
+    def dicomToPNG(dicom_folder,
+                   output_folder,
+                   dic_extension = "dcm",
+                   grey = True,
+                   coef_max = 255.0):
+        """Converts DICOM file arrays to PNG images
+        and places them into a target folder. 
+        By default, it interprets them as greyscale"""
+        
+        if not os.path.exists(output_folder):
+            os.mkdir(output_folder)
+            
+        list_files_temp = os.listdir(dicom_folder)
+        # Narrow the list to only include files with our desired extension
+        list_files = list()
+        for i in list_files_temp:
+            if i[-3:] == dic_extension:
+                list_files.append(i)
+        
+        for filename in list_files:
+            ds = pydicom.dcmread(os.path.join(dicom_folder + filename))
+            shape = ds.pixel_array.shape
+            # Convert to float to avoid overflow or underflow losses.
+            image_2d = ds.pixel_array.astype(float)
+            image_2d_scaled = (np.maximum(image_2d,0) / image_2d.max()) * coef_max
+            # Convert to uint
+            image_2d_scaled = np.uint8(image_2d_scaled)
+            p_filename = filename[:-3] + "png"
+    
+        with open(os.path.join(output_folder + p_filename), 'wb') as png_file:
+            w = png.Writer(shape[1], shape[0], greyscale=grey)
+            w.write(png_file, image_2d_scaled)
+            
+        return os.listdir(output_folder)
+            
--- setup.py
+++ setup.py
@@ -12,14 +12,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='imagewiz',
-                 version='0.3',
+                 version='0.4',
                  author="Evan Gibson",
                  author_email="evan.gibson@duofinch.com",
                  description="A image preprocessing module to compliment Tensorflow/Keras",
                  long_description=long_description,
                  long_description_content_type="text/markdown",
-                 url="https://github.com/evangibson/imagewiz",
+                 url="https://github.com/DuoFinch/imagewiz",
                  packages=['imagewiz',],
-                 classifiers=["Programming Language :: Python :: 3",
+                 install_requires=['pydicom', 'png', 'tqdm'],
+                 classifiers=["Development Status :: 2 - Pre-Alpha",
+                              "Programming Language :: Python :: 3",
                               "License :: OSI Approved :: MIT License",
-                              "Operating System :: OS Independent"])
+                              "Operating System :: OS Independent",
+                              "Topic :: Scientific/Engineering :: Image Recognition"])
